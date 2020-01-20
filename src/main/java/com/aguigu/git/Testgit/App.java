c03ed5db1fb409b1c1d03fdaea5b8e87f24a987e
glouphtian <richi_82@163.com>
2020-01-20 14:01:39 +0800

    My first commit by STS

---
+++ src/main/java/com/aguigu/git/Testgit/App.java
@@ -0,0 +1,13 @@
+package com.aguigu.git.Testgit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
